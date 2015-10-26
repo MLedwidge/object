1086e3f4569785396fe0e86dd93ca971fb333a84
MLedwidge <mattledwidge@gmail.com>
2015-10-26 19:26:22 -0400

    upload

---
+++ objects.js
@@ -0,0 +1,16 @@
+
+{
+		"name":"Planet",
+		"stuffs":{
+				current:{
+						"title":"Earth",
+						"era":"Anthropocene",
+						"dates":"after 2015",
+					},
+					previous:[
+						{"title":"boom","era":"Cenozoic","dates":"66 million years ago to 2015"},
+						{"title":"Clerk","company":"Mesozoic","dates":"252 million years ago to 66 million years ago"},
+						{"title":"Clerk","company":"paleozoic","dates":"541 million years ago to 242 million years ago"}
+						]
+					}
+};
